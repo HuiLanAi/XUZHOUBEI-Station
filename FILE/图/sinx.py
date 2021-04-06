d851e3df3f55d80b7241d376436a0360574055e3
HuiLanAi <310-we-aaa-1@163.com>
2021-04-06 19:34:36 +0800

    Initial commit

---
+++ FILE/图/sinx.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+x = np.linspace(-2*np.pi, 2*np.pi, 1000)
+y = np.sin(x)
+z = np.sin(x+0.5*np.pi)
+k = np.sin(0*x)
+p = np.sin(x*0.6)
+plt.plot(x, y)
+plt.plot(x, z)
+plt.plot(x, p, ls='--')
+plt.plot(x, k, color='black')
